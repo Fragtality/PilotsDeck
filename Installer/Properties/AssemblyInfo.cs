1f6a432649a67bcd95a7396df8b8cf5601f8ffa6
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-05-23 19:14:27 +0200

    Some Log Messages for Installer / Serilog Package Update

--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("PilotsDeck Installer v0.8.9.0 (2025.05.18.1327)")]
+[assembly: AssemblyTitle("PilotsDeck Installer v0.8.9.0 (2025.05.23.1713)")]
 [assembly: AssemblyDescription("Installer Application for PilotsDeck")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
